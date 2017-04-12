875028ec0542da150dec54cdda2501d7f48f219d
FelipeMSX <felipemsx18@gmail.com>
2017-04-12 04:00:41 -0300

    f

--- Projeto Cadimuns/principal.Designer.cs
+++ Projeto Cadimuns/principal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Projeto_Cadimuns
 {
-	partial class Form1
+	partial class principal
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,9 +28,17 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// principal
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(419, 307);
+			this.Name = "principal";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
